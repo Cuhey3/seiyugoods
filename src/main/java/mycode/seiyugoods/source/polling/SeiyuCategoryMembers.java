224643e85019e2ddbe0cb56ccd8f8b73c1d2f592
cuhey <cuhey@nanashi-pc>
2015-08-20 21:50:23 +0900

    lombokのバージョンを最新に戻し、@Builderが悪さしているかどうかを確認

--- src/main/java/mycode/seiyugoods/source/polling/SeiyuCategoryMembers.java
+++ src/main/java/mycode/seiyugoods/source/polling/SeiyuCategoryMembers.java
@@ -1,5 +1,6 @@
 package mycode.seiyugoods.source.polling;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import mycode.seiyugoods.source.PollingSource;
@@ -16,12 +17,13 @@ public class SeiyuCategoryMembers extends PollingSource {
     @Override
     public Map<String, Object> poll() throws Exception {
         Map<String, Object> sourceFields = new HashMap<>();
-        sourceFields.put("mapList", WikiParse.builder()
+        /*sourceFields.put("mapList", WikiParse.builder()
                 .param("action=query&list=categorymembers&cmtitle=Category:%E6%97%A5%E6%9C%AC%E3%81%AE%E5%A5%B3%E6%80%A7%E5%A3%B0%E5%84%AA&cmlimit=500&cmnamespace=0&format=xml&continue=&cmprop=title|sortkeyprefix")
                 .list("categorymembers")
                 .map("cm")
                 .continueElement("cmcontinue")
-                .build().getMapList());
+                .build().getMapList());*/
+        sourceFields.put("mapList", new ArrayList<>());
         return sourceFields;
     }
 }
